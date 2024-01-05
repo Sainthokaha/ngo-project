4761d2fdb7437096c034af5e4611bf0c12e1bea2
sainthokaha5@gmail.com <88292276+Sainthokaha@users.noreply.github.com>
2024-01-05 01:40:34 +0100

    image-slider

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
   import TopContact from './components/TopContact.vue'
   import CharityHeader from './components/CharityHeader.vue'
   import NavBar from './components/NavBar.vue'
+  import ImageSlider from './components/ImageSlider.vue'
 </script>
 
 <template>
   <TopContact />
   <CharityHeader />
   <NavBar />
+  <ImageSlider />
 </template>
 
 <style scoped>
---
+++ src/components/ImageSlides.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="slides">
+    {{image.title}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      "image"
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
