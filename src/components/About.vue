11a8e4d58286e5918808168b978c37a39bdb9d13
sainthokaha5@gmail.com <88292276+Sainthokaha@users.noreply.github.com>
2024-05-26 00:10:56 +0100

    about-link commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,13 @@
 <template>
-  <div>
+  <div class="w-full">
     <div class="about-container flex justify-around mt-20">
-      <div class="upcoming w-[45%]">
-        <div class="heading uppercase font-bold text-[20px] mb-10">
-          Upcoming Events
-        </div>
+      <div class="upcoming w-[36%]">
+        <div class="heading uppercase font-bold text-[20px] mb-10">Upcoming Events</div>
         <div class="upcoming-container flex items-center my-6">
           <div class="left flex">
-            <img
-              class="w-[420px] h-28 aboslute"
-              src="../assets/upcoming-img.jpg"
-              alt=""
-            />
+            <img class="w-[420px] h-28 aboslute" src="../assets/about-image.jpg" alt="" />
             <div class="relative -left-[53px]">
-              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">
-                28
-              </div>
+              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">28</div>
               <div
                 class="month uppercase bg-slate-950 text-white text-[12px] p-2 text-center"
               >
@@ -40,23 +32,17 @@
               </div>
             </div>
             <p class="text-[14px] text-gray-400 font-[600]">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis suscipit necessitatibus iure, repudiandae vitae odio
-              ipsum quam eligendi minima voluptates.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+              suscipit necessitatibus iure, repudiandae vitae odio ipsum quam eligendi
+              minima voluptates.
             </p>
           </div>
         </div>
         <div class="upcoming-container flex items-center my-6">
           <div class="left flex">
-            <img
-              class="w-[420px] h-28 aboslute"
-              src="../assets/upcoming-img.jpg"
-              alt=""
-            />
+            <img class="w-[420px] h-28 aboslute" src="../assets/about-image.jpg" alt="" />
             <div class="relative -left-[53px]">
-              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">
-                28
-              </div>
+              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">28</div>
               <div
                 class="month uppercase bg-slate-950 text-white text-[12px] p-2 text-center"
               >
@@ -81,23 +67,17 @@
               </div>
             </div>
             <p class="text-[14px] text-gray-400 font-[600]">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis suscipit necessitatibus iure, repudiandae vitae odio
-              ipsum quam eligendi minima voluptates.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+              suscipit necessitatibus iure, repudiandae vitae odio ipsum quam eligendi
+              minima voluptates.
             </p>
           </div>
         </div>
         <div class="upcoming-container flex items-center my-6">
           <div class="left flex">
-            <img
-              class="w-[420px] h-28 aboslute"
-              src="../assets/upcoming-img.jpg"
-              alt=""
-            />
+            <img class="w-[420px] h-28 aboslute" src="../assets/about-image.jpg" alt="" />
             <div class="relative -left-[53px]">
-              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">
-                28
-              </div>
+              <div class="date bg-[#f14b05] text-white text-[20px] py-3 px-4">28</div>
               <div
                 class="month uppercase bg-slate-950 text-white text-[12px] p-2 text-center"
               >
@@ -122,34 +102,31 @@
               </div>
             </div>
             <p class="text-[14px] text-gray-400 font-[600]">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis suscipit necessitatibus iure, repudiandae vitae odio
-              ipsum quam eligendi minima voluptates.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+              suscipit necessitatibus iure, repudiandae vitae odio ipsum quam eligendi
+              minima voluptates.
             </p>
           </div>
         </div>
       </div>
-      <div class="featured w-[45%]">
-        <div class="heading uppercase font-bold text-[20px] mb-10">
-          Featured Causes
-        </div>
+      <div class="featured w-[36%]">
+        <div class="heading uppercase font-bold text-[20px] mb-10">Featured Causes</div>
         <div class="featured-container flex">
           <div class="img-container">
-            <img class="h-[410px] w-[800px]" src="../assets/featured-img.jpg" alt="" />
+            <img class="h-[410px] w-[1300px]" src="../assets/featured-img.jpg" alt="" />
           </div>
-          <div class="writeup">
-            <h1 class="uppercase font-[500] mb-10">charity for education</h1>
-            <p class="text-[#f14b05] text-[12px] font-[800]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-              aliquid maiores laborum.
+          <div class="writeup flex flex-col items-center justify-center">
+            <h1 class="uppercase text-[18px] font-[500] mb-6">charity for education</h1>
+            <p class="text-[#f14b05] text-[12px] font-[800] mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto aliquid
+              maiores laborum.
             </p>
-            <p class="text-[14px] text-gray-400 font-[600]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-              voluptatem atque esse at eligendi? Atque iste tenetur debitis.
-              Earum odit atque ipsam reprehenderit ducimus a aut, provident enim
-              iusto sed.
+            <p class="text-[12px] text-gray-400 font-[600] mb-6">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem
+              atque esse at eligendi? Atque iste tenetur debitis. Earum odit atque ipsam
+              reprehenderit ducimus a aut, provident enim iusto sed.
             </p>
-            <div class="btn">
+            <div class="btn mb-6">
               <button
                 class="text-xs font-bold border-2 border-[#f14b05] text-white px-5 py-2 rounded-full hover:text-[#f14b05] bg-[#f14b05] hover:bg-white hover:transition-all mr-2"
               >
@@ -161,22 +138,82 @@
                 READ MORE
               </button>
             </div>
-            <h2>help us share:</h2>
+            <h2 class="uppercase font-[500] text-[14px]">help us share:</h2>
             <div class="icons flex">
-              <div>
-                <img class="w-5 h-5" src="../assets/facebook (2).svg" alt="">
+              <div class="about-link">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    fill="#f14b05"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M15.997 3.985h2.191V.169C17.81.117 16.51 0 14.996 0c-3.159 0-5.323 1.987-5.323 5.639V9H6.187v4.266h3.486V24h4.274V13.267h3.345l.531-4.266h-3.877V6.062c.001-1.233.333-2.077 2.051-2.077z"
+                    />
+                  </svg>
+                </a>
               </div>
-              <div>
-                <img class="w-5 h-5" src="../assets/twitter (1).svg" alt="">
+              <div class="about-link">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    fill="#f14b05"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M21.534 7.113A9.822 9.822 0 0 0 24 4.559v-.001c-.893.391-1.843.651-2.835.777a4.894 4.894 0 0 0 2.165-2.719 9.845 9.845 0 0 1-3.12 1.191 4.919 4.919 0 0 0-8.511 3.364c0 .39.033.765.114 1.122-4.09-.2-7.71-2.16-10.142-5.147a4.962 4.962 0 0 0-.674 2.487c0 1.704.877 3.214 2.186 4.089A4.863 4.863 0 0 1 .96 9.116v.054a4.943 4.943 0 0 0 3.942 4.835c-.401.11-.837.162-1.29.162-.315 0-.633-.018-.931-.084.637 1.948 2.447 3.381 4.597 3.428a9.89 9.89 0 0 1-6.101 2.098c-.403 0-.79-.018-1.177-.067a13.856 13.856 0 0 0 7.548 2.208c8.683 0 14.342-7.244 13.986-14.637z"
+                    />
+                  </svg>
+                </a>
               </div>
-              <div>
-                <img class="w-5 h-5" src="../assets/google-plus-g.svg" alt="">
+              <div class="about-link">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    fill="#f14b05"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M21.823 9h-2.187v2.177h-2.177v2.187h2.177v2.177h2.187v-2.177H24v-2.187h-2.177zM7.5 19.5c4.328 0 7.203-3.038 7.203-7.326 0-.491-.051-.87-.122-1.248h-7.08v2.578h4.257c-.174 1.095-1.289 3.233-4.257 3.233-2.557 0-4.645-2.118-4.645-4.737s2.087-4.738 4.645-4.738c1.463 0 2.435.624 2.988 1.156l2.036-1.954C11.214 5.237 9.526 4.5 7.5 4.5 3.356 4.5 0 7.856 0 12s3.356 7.5 7.5 7.5z"
+                    />
+                  </svg>
+                </a>
               </div>
-              <div>
-                <img class="w-5 h-5" src="../assets/pinterest.svg" alt="">
+              <div class="about-link">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    fill="#f14b05"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M7.041 24a.666.666 0 0 1-.644-.543c-.098-.564-.558-3.429-.101-5.31.239-.981 1.421-5.805 1.657-6.767a5.616 5.616 0 0 1-.406-2.117c0-2.182 1.371-3.891 3.122-3.891 1.392 0 2.327.99 2.327 2.462 0 .838-.331 1.869-.682 2.961-.191.596-.39 1.212-.534 1.801-.112.47-.022.923.255 1.275.313.399.826.627 1.405.627 2.345 0 4.046-2.475 4.046-5.884 0-3.046-2.281-5.173-5.547-5.173-3.919 0-6.033 2.862-6.033 5.555 0 1.022.394 2.196.957 2.854a.905.905 0 0 1 .202.844l-.133.521c-.113.442-.24.939-.278 1.076a.773.773 0 0 1-.405.543c-.22.107-.478.095-.768-.037-2.021-.901-3.231-3.684-3.231-5.852C2.25 4.637 5.398.001 12.31.001V0c5.365 0 9.424 3.676 9.44 8.551 0 5.232-3.444 9.177-8.012 9.177-1.158 0-2.313-.417-3.083-1.061l-.654 2.413c-.535 1.992-2.422 4.631-2.44 4.655a.648.648 0 0 1-.52.265zm-.277-.869-.002.002.002-.002zM10.67 6.374c-1.19 0-2.122 1.27-2.122 2.891 0 1.085.374 1.833.377 1.84a.505.505 0 0 1 .041.348s-1.432 5.841-1.697 6.933c-.293 1.206-.147 2.964-.016 4.02.577-.902 1.473-2.431 1.783-3.584l.9-3.322a.5.5 0 0 1 .921-.11c.361.659 1.537 1.339 2.881 1.339 3.997 0 7.012-3.515 7.012-8.176C20.735 4.248 17.106 1 12.31 1c-6.225.001-9.06 4.119-9.06 7.945 0 1.755.95 4.068 2.497 4.872.06-.228.142-.551.218-.848l.132-.517c-.708-.786-1.189-2.194-1.189-3.454 0-3.178 2.465-6.555 7.033-6.555 3.854 0 6.547 2.539 6.547 6.173 0 3.989-2.122 6.884-5.046 6.884-.888 0-1.686-.368-2.191-1.008-.471-.597-.627-1.354-.44-2.129.153-.626.356-1.259.554-1.872.326-1.015.634-1.974.634-2.655-.002-.44-.131-1.462-1.329-1.462z"
+                    />
+                  </svg>
+                </a>
               </div>
-              <div>
-                <img class="w-5 h-5" src="../assets/social-youtube.svg" alt="">
+              <div class="about-link">
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    fill="#f14b05"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="m23.469 5.929.03.196c-.49-1.738-1.989-2.056-2.089-2.117-2.434-.661-16.298-.686-18.799 0C.896 4.505.581 6.025.522 6.125c-.699 3.651-.704 8.038.031 11.947l-.031-.198c.49 1.738 1.989 2.056 2.089 2.117 2.467.672 16.295.674 18.799 0 1.715-.496 2.03-2.017 2.089-2.117.653-3.474.696-8.003-.03-11.945zM9.608 15.651V8.358l6.266 3.652z"
+                    />
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
@@ -189,19 +226,40 @@
 <script>
 export default {
   data() {
-      return {
-        isHovered: false
-      }
+    return {
+      isHovered: false,
+    };
+  },
+  methods: {
+    onMouseEnter() {
+      this.isHovered = true;
     },
-    methods: {
-      onMouseEnter() {
-        this.isHovered = true;
-      },
-      onMouseLeave() {
-        this.isHovered = false;
-      }
-    }
+    onMouseLeave() {
+      this.isHovered = false;
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .about-link a {
+    border: 1px solid #CACFD3;
+    border-radius: 20px;
+    height: 40px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 9px;
+    transition: all .3s ease;
+  }
+
+  .about-link a:hover {
+    background-color: #f14b05;
+    transition: all .3s ease;
+  }
+
+  .about-link a:hover svg {
+    fill: white;
+  }
+</style>
